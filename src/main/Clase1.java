c82789caecca5eb2c9efb2a225e0706a6a8b9c0d
sfrisuelos <sfrisuelos@gmail.com>
2026-02-16 13:02:03 +0100

    Segunda carga

---
+++ src/main/Clase1.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Clase1 {
+
+	public Clase1() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	//Clase 1
+	
+	
+}
---
+++ src/main/Clase2.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Clase2 {
+
+	public Clase2() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	//Clase 2
+}
